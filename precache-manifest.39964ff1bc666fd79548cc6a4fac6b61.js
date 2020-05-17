b9825ec6d689100889e2a47aacf3bdb2af48a166
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-05-16 23:05:59 -0500

    Updates

---
+++ precache-manifest.39964ff1bc666fd79548cc6a4fac6b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14146aa83602b52d7dfe05fd3c7d323",
+    "url": "/TaniaGM/reactportfolio/index.html"
+  },
+  {
+    "revision": "5744e7f5fac773fe04b3",
+    "url": "/TaniaGM/reactportfolio/static/css/main.a9c64b88.chunk.css"
+  },
+  {
+    "revision": "7659d003f257daa1f5f4",
+    "url": "/TaniaGM/reactportfolio/static/js/2.452d101b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TaniaGM/reactportfolio/static/js/2.452d101b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5744e7f5fac773fe04b3",
+    "url": "/TaniaGM/reactportfolio/static/js/main.373104c0.chunk.js"
+  },
+  {
+    "revision": "06b17b4ab60d9578514c",
+    "url": "/TaniaGM/reactportfolio/static/js/runtime-main.5c3a0eab.js"
+  }
+]);
