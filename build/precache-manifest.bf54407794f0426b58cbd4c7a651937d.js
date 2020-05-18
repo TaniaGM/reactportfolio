66379e86906e288c74f56a253ec8eaab0de3c170
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-05-17 21:53:57 -0500

    run build

---
+++ build/precache-manifest.bf54407794f0426b58cbd4c7a651937d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381ea9e38161bffd3e00acb0b53e8bff",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "248bb62f252b153cbb4d",
+    "url": "/reactportfolio/static/css/main.2f55e03d.chunk.css"
+  },
+  {
+    "revision": "3d3c35bb7f82becb88ea",
+    "url": "/reactportfolio/static/js/2.3d26b780.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio/static/js/2.3d26b780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248bb62f252b153cbb4d",
+    "url": "/reactportfolio/static/js/main.71b717c3.chunk.js"
+  },
+  {
+    "revision": "a5d6de1384606c80a610",
+    "url": "/reactportfolio/static/js/runtime-main.8dc71b87.js"
+  }
+]);
